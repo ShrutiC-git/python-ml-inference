6e32c0da9161ff8c3a98369dfb44e0d6dc050863
Shruti Chaturvedi <66940685+ShrutiC-git@users.noreply.github.com>
2025-09-27 09:35:45 +0530

    add minio

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ import joblib, os
 
 app = FastAPI()
 
-MODEL_PATH = "/artifacts/model.pkl"
+# MODEL_PATH = "/artifacts/model.pkl"
 
-# Load Model on startup
-model = None
-if os.path.exists(MODEL_PATH):
-    model = joblib.load(MODEL_PATH)
-else: 
-    print("No model found yet. Run fraud-trainer svc to generate a model...")
+# # Load Model on startup
+# model = None
+# if os.path.exists(MODEL_PATH):
+#     model = joblib.load(MODEL_PATH)
+# else: 
+#     print("No model found yet. Run fraud-trainer svc to generate a model...")
+
+client = Minio(
+    "minio.infra.svc.cluster.local:9000",
+    access_key=os.getenv("MINIO_ACCESS_KEY"),
+    secret_key=os.getenv("MINIO_SECRET_KEY"),
+    secure=False
+)
+
+BUCKET = "fraud-models"
+client.fget_object(BUCKET, "model.pkl", "/tmp/model.pkl")
+
+model = joblib.load("/tmp/model.pkl")
 
 @app.get("/predict")
 def predict(amount: float = Query(..., description="Transaction amount")):
